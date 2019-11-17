2bb721e9a8eeeef2501be3fcf2f3dc696277f168
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-17 19:32:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  avengers = []
 name_hash.collect do |name, value|
-
+ avengers << value
+ if avengers[0] > av
 end
 end
