d286dc5d60c80338d73660b2bebe0bda30cb9a5b
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-17 19:25:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 
 def key_for_min_value(name_hash)
   define = 600
-name_hash.collect do |key, value|
+name_hash.collect do |name, value|
   if value < define
-    define = key
+    define = namey
   end
   define
 end
