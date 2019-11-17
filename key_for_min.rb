728cf87f9f6e54ebff35f91ccd6a7b0f0e91d8b1
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-17 19:41:59 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-end
+  avengers = nil 
+  heroes = nil
+name_hash.each do |name, value|
+ if heroes == nil || value < heroes
+   heroes = value
+   avengers = name
+ end
+ end
+ avengers
+ end
