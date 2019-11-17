0a12b1edd10acbf44872c688af294a2be8650509
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-17 19:41:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,18 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  avengers = []
-name_hash.collect do |name, value|
- avengers << value
- if avengers[0] > avengers[1]
-   if avengers[0] > avengers[2]
-     puts avengers[0]
-   else puts avengers[2]
-   end
- elsif avengers[1] > avengers[2]
-       puts avengers[1]
- else
-     puts avengers[3]
-end
-end
-end
+  avengers = nil 
+  heroes = nil
+name_hash.each do |name, value|
+ if heroes == nil || value < heroes
+   heroes = value
+   avengers = name
+ end
+ end
+ avengers
+ end
