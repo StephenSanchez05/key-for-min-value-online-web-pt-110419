4d96bec6ddf170a8b3201ae35db931b8cee006d5
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-17 19:36:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,12 @@ def key_for_min_value(name_hash)
 name_hash.collect do |name, value|
  avengers << value
  if avengers[0] > avengers[1]
-   if
+   if avengers[0] > avengers[2]
+     puts avengers[0]
+   else puts avengers[2]
+ elsif avengers[1] > avengers[2]
+       puts avengers[1]
+ else
+     puts avengers[3]
 end
 end
